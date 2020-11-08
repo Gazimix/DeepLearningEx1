ad8a88ec30ee75466167d4018e9bb0d2e3c8d4dd
Chend <chendetails@gmail.com>
2020-11-08 14:25:06 +0200

    Started working on the leaning algorithm - thus far only copied the text from the website, will son start customizing to the exercise.

--- main.py
+++ main.py
@@ -1,2 +1,90 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+EPOCHS = 5
+
+
+class MyModel(Model):
+	def __init__(self):
+		super(MyModel, self).__init__()
+		self.conv1 = Conv2D(32, 3, activation='relu')
+		self.flatten = Flatten()
+		self.d1 = Dense(128, activation='relu')
+		self.d2 = Dense(10)
+	
+	def call(self, x):
+		x = self.conv1(x)
+		x = self.flatten(x)
+		x = self.d1(x)
+		return self.d2(x)
+
+
+model = MyModel()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+
+@tf.function
+def train_step(sequences, labels):
+	with tf.GradientTape() as tape:
+		# training=True is only needed if there are layers with different
+		# behavior during training versus inference (e.g. Dropout).
+		predictions = model(sequences, training=True)
+		loss = loss_object(labels, predictions)
+	gradients = tape.gradient(loss, model.trainable_variables)
+	optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+	
+	train_loss(loss)
+	train_accuracy(labels, predictions)
+
+
+@tf.function
+def test_step(sequences, labels):
+	# training=False is only needed if there are layers with different
+	# behavior during training versus inference (e.g. Dropout).
+	predictions = model(sequences, training=False)
+	t_loss = loss_object(labels, predictions)
+	test_loss(t_loss)
+	test_accuracy(labels, predictions)
+
+
+optimizer = tf.keras.optimizers.Adam()
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
 if __name__ == '__main__':
-    pass
+
+	mnist = tf.keras.datasets.mnist
+	
+	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	x_train, x_test = x_train / 255.0, x_test / 255.0
+	
+	# Add a channels dimension
+	x_train = x_train[..., tf.newaxis].astype("float32")
+	x_test = x_test[..., tf.newaxis].astype("float32")
+	
+	train_ds = tf.data.Dataset.from_tensor_slices(
+		(x_train, y_train)).shuffle(10000).batch(32)
+	
+	test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+	
+	for epoch in range(EPOCHS):
+		# Reset the metrics at the start of the next epoch
+		train_loss.reset_states()
+		train_accuracy.reset_states()
+		test_loss.reset_states()
+		test_accuracy.reset_states()
+		
+		for sequences, labels in train_ds:
+			train_step(sequences, labels)
+		
+		for test_sequences, test_labels in test_ds:
+			test_step(test_sequences, test_labels)
+		
+		print(f'Epoch {epoch + 1}, '
+			  f'Loss: {train_loss.result()}, '
+			  f'Accuracy: {train_accuracy.result() * 100}, '
+			  f'Test Loss: {test_loss.result()}, '
+			  f'Test Accuracy: {test_accuracy.result() * 100}')
