4b1a3483ebc139a2ddd97a273ced1f0b1b32089b
Dor <18174994+ScDor@users.noreply.github.com>
2020-11-23 14:06:11 +0200

    broken tuple

--- Ex1.py
+++ Ex1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Flatten, Activation
 
 EPOCHS = 3
@@ -10,7 +9,7 @@ char_to_int = {c: i for i, c in enumerate(AMINOS)}
 int_to_char = {i: c for i, c in enumerate(AMINOS)}
 
 
-class MyModel(Model):
+class MyModel(tf.keras.Model):
     def __init__(self):
         super(MyModel, self).__init__()
         self.flatten = Flatten()
@@ -27,66 +26,60 @@ class MyModel(Model):
         x = self.a(x)
         return x
 
+    def train_step(self, data):
+        # Unpack the data. Its structure depends on your model and
+        # on what you pass to `fit()`.
+        x, y, sample_weight = data
 
-model = MyModel()
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)  # Forward pass
+            # Compute the loss value.
+            # The loss function is configured in `compile()`.
+            loss = self.compiled_loss(y, y_pred, sample_weight=sample_weight,
+                                      regularization_losses=self.losses)
 
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
 
-@tf.function
-def train_step(model: MyModel, sequences, labels):
-    with tf.GradientTape() as tape:
-        # training=True is only needed if there are layers with different
-        # behavior during training versus inference (e.g. Dropout).
-        predictions = model(sequences, training=True)
-        loss = loss_object(labels, predictions)
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    train_loss(loss)
-    train_accuracy(labels, predictions)
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
 
+        # Update the metrics.
+        # Metrics are configured in `compile()`.
+        self.compiled_metrics.update_state(y, y_pred, sample_weight=sample_weight)
 
-@tf.function
-def test_step(model:MyModel, sequences, labels):
-    # training=False is only needed if there are layers with different
-    # behavior during training versus inference (e.g. Dropout).
-    predictions = model(sequences, training=True)
-    t_loss = loss_object(labels, predictions)
-    test_loss(t_loss)
-    test_accuracy(labels, predictions)
+        # Return a dict mapping metric names to current value.
+        return {m.name: m.result() for m in self.metrics}
 
+    def test_step(self, data):
+        x, y = data
+        y_pred = self(x, training=False)
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
-test_loss = tf.keras.metrics.Mean(name='test_loss')
-test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+        # Updates the metrics tracking the loss
+        self.compiled_loss(y, y_pred, regularization_losses=self.losses)
 
+        # Update the metrics.
+        self.compiled_metrics.update_state(y, y_pred)
+        # Return a dict mapping metric names to current value.
+        # Note that it will include the loss (tracked in self.metrics).
+        return {m.name: m.result() for m in self.metrics}
 
-def train_and_appraise(model:MyModel,train_ds, test_ds):
-    """
-    Trains the data with the GD method and outputs the loss, accuracy, test loss and test accuracy achieved
-    during the process.
-    :param train_ds:
-    :param test_ds:
-    """
-    for epoch in range(EPOCHS):
-        # Reset the metrics at the start of the next epoch
-        train_loss.reset_states()
-        train_accuracy.reset_states()
-        test_loss.reset_states()
-        test_accuracy.reset_states()
+    def train_and_appraise(self, train_ds, test_ds, weights):
+        """
+        Trains the data with the GD method and outputs the loss, accuracy, test loss and test accuracy achieved
+        during the process.
+        :param train_ds:
+        :param test_ds:
+        """
+        for epoch in range(EPOCHS):
+            for sequences, labels in train_ds:
+                self.train_step((sequences, labels, weights))
 
-        for sequences, labels in train_ds:
-            train_step(model, sequences, labels)
+            for test_sequences, test_labels in test_ds:
+                self.test_step((test_sequences, test_labels))
 
-        for test_sequences, test_labels in test_ds:
-            test_step(model, test_sequences, test_labels)
-
-        print(f'Epoch {epoch + 1}, '
-              f'Loss: {train_loss.result()}, '
-              f'Accuracy: {train_accuracy.result() * 100}, '
-              f'Test Loss: {test_loss.result()}, '
-              f'Test Accuracy: {test_accuracy.result() * 100}')
+            print(self.losses)
 
 
 POSITIVE = r"positive"
@@ -132,19 +125,18 @@ def get_data():
     the sample and the label
     """
     (x_train, y_train), (x_test, y_test) = data_as_tensor()
-    # Add a channels dimension
-    # x_train = x_train[..., tf.newaxis]
-    # x_test = x_test[..., tf.newaxis]
     train_ds = tf.data.Dataset.from_tensor_slices(
         (x_train, y_train)).shuffle(10000).batch(32)
     test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
-    return train_ds, test_ds
+    weights = np.array([1 if y else 3 / 27 for y in y_train])
+    weights = np.expand_dims(weights,axis=1)
+    return train_ds, test_ds, weights
 
 
 def get_9_mers():
     string = ""
     with open(r"predict") as pred:
-        lines = pred.read().replace("\n","").strip()
+        lines = pred.read().replace("\n", "").strip()
     encoded_k, nine_mers = [], []
 
     for i in range(len(lines.strip()) - 8):
@@ -157,10 +149,11 @@ def get_9_mers():
 
 
 if __name__ == '__main__':
-    train_ds, test_ds = get_data()
     aggregated = {}
+    model = MyModel()
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='mae')
+    model.train_and_appraise(*get_data())
 
-    train_and_appraise(model, train_ds, test_ds)
     encoded, nine_mers = get_9_mers()
     predict = model.predict(encoded)
 
@@ -170,9 +163,6 @@ if __name__ == '__main__':
     predicted_false = [x for i, x in enumerate(nine_mers) if not predictions[i]]
     results = [(nine_mers[i], predictions[i], max(predict[i])) for i in
                range(len(predict))]
-    # for r in results:
-    #     if not r[1]:
-    #         print(r)
 
     results.sort(key=lambda tup: tup[2], reverse=True)
     print(f"epoch count ={EPOCHS}")
